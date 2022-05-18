32b1c631f988e844c37cee9fcb7a1a11fb0b8bf2
Gary Smith <garysmith193358@gmail.com>
2022-05-17 22:49:55 -0400

    initial commit

---
+++ src/components/Countdown.js
@@ -0,0 +1,65 @@
+import React, {useState, useEffect} from 'react'
+import { Text, View, StyleSheet } from "react-native"
+import {fontSizes, spacing} from '../Utils/sizes';
+import { colors } from '../Utils/colors'
+
+const minutesToMillis = (min) => min * 1000 * 60;
+const formatTime = (time) => time < 10 ? `0${time}` : time
+
+export const Countdown = ({
+  minutes = 20,
+  isPaused,
+  onProgress,
+  onEnd
+}) => {
+  const interval = React.useRef(null)
+
+  const countDown = () => {
+    setMillis((time) => {
+      if(time === 0) {
+        clearInterval(interval.current);
+        onEnd();
+        return time;
+      }
+      const timeLeft = time - 1000
+      
+      return timeLeft;
+    })
+  }
+  const [millis, setMillis] = useState(minutesToMillis(null))
+
+
+  useEffect(() => {
+    onProgress(timeLeft / minutesToMillis(minutes))
+  }, [millis])
+
+  useEffect(() => {
+    setMillis(minutesToMillis(minutes))
+  }, [minutes])
+
+useEffect(() => {
+  if(isPaused) {
+    if(interval.current) clearInterval(interval.current);
+    return 
+  }
+  interval.current = setInterval(countDown, 1000);
+
+  return () => clearInterval(interval.current)
+}, [isPaused])
+
+  const minute = Math.floor(millis / 1000 / 60) % 60;
+  const seconds = Math.floor(millis / 1000) % 60;
+  return (
+    <Text style={styles.text}> {formatTime(minute)}:{formatTime(seconds)} </Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: fontSizes.xxxxl,
+    fontWeight: 'bold',
+    color: colors.white,
+    padding: spacing.lg,
+    backgroundColor: 'rgba(94, 132, 226, 0.3)'
+  }
+})
---
+++ src/features/focus/FocusHistory.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, StyleSheet, FlatList, Text, SafeAreaView } from 'react-native'
+
+import {fontSizes, spacing} from "../../Utils/sizes"
+import { RoundedButton } from "../../components/RoundedButton"
+
+const HistoryItem = ({item, index}) => {
+ return (
+  <Text style={styles.historyItem(item.status)}>
+    {JSON.stringify(item.subject)}
+  </Text>
+ )
+}
+
+export const FocusHistory = ({focusHistory, onClear}) => {
+  const clearHistory = () => {
+    onClear();
+  }
+
+  return (
+    <>
+    <SafeAreaView style={{flex: 0.5, alignItems: 'center'}}>
+      {!!focusHistory.length && (
+        <>
+      <Text style={styles.title}> Things we've focused on </Text> 
+        <FlatList 
+        style={{width: '100%', height: '100%'}}
+        contentContainerStyle={{flex: 1, alignItems: 'center'}}
+        data={focusHistory}
+        renderItem={HistoryItem}
+      />
+
+        <View style={styles.clearContainer}> 
+          <RoundedButton size={75} title='Clear' onPress={() => onClear()}/>
+        </View>
+         </>
+
+      )}
+    </SafeAreaView>
+   
+    </>  
+  )
+}
+
+const styles = StyleSheet.create({
+  historyItem: (status) => ({
+    color: status > 1 ? 'red' : 'green',
+    fontSize: fontSizes.medium
+  }),
+  title: {
+     color: 'white',
+    fontSize: fontSizes.lg
+  },
+  clearContainer: {
+    alignItems: 'center',
+    padding: spacing.md
+  }
+})
